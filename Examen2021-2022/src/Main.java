adf57a6f3b5b2ca10b9e4f9df8dfff9386e55010
Mamadou Mbaye <125107008+521653@users.noreply.github.com>
2023-10-17 21:21:58 -0100

    Add files via upload

---
+++ Examen2021-2022/src/Main.java
@@ -0,0 +1,12 @@
+import MesClasses.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Achat> liste = new ArrayList<>();
+
+        Facture fature = new Facture(LocalDate.of(2022,5,4),new ArrayList<>());
+    }
+}
---
+++ Examen2021-2022/src/MesClasses/Facture.java
@@ -0,0 +1,50 @@
+package MesClasses;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Facture {
+    private static int numFact = 0;
+    private LocalDate date;
+    private ArrayList<Achat> listeAchat;
+    public Facture(){}
+    public Facture(LocalDate date, ArrayList<Achat> listeAchat){
+        this.date = date;
+        this.listeAchat = listeAchat;
+    }
+    public boolean verifier(Achat a){
+        int i;
+        for ( i = 0; i < listeAchat.size(); i++) {
+            if (listeAchat.get(i).equals(a)){
+                break;
+            }
+        }
+        if (i < listeAchat.size())
+            return true;
+        else
+            return false;
+
+    }
+    public void ajouter(Achat a){
+
+        if (verifier(a))
+            System.out.println("l'achat existe dans la liste !");
+        else
+            listeAchat.add(a);
+    }
+    public void supprimer(Achat a){
+        if (verifier(a))
+            listeAchat.remove(a);
+        else
+            System.out.println("l'achat n'existe pas dans la liste !");
+    }
+    public double montantFacture(){
+        double s =0;
+        for (Achat val : listeAchat)
+            s+= val.montantAchat();
+        return s;
+    }
+    public String toString(){
+    return "numero : "+numFact+", liste de achats : "+listeAchat+", montant total de la facture : "+montantFacture();
+    }
+}
