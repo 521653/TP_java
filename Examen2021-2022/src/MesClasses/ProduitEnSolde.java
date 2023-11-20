e4f3d97285eb1d9193619fcaed748680e96e2d1d
Mamadou Mbaye <125107008+521653@users.noreply.github.com>
2023-11-20 15:05:20 +0000

    Update ProduitEnSolde.java

--- Examen2021-2022/src/MesClasses/ProduitEnSolde.java
+++ Examen2021-2022/src/MesClasses/ProduitEnSolde.java
@@ -2,7 +2,10 @@ package MesClasses;
 
 public class ProduitEnSolde extends Produit{
     private double prime;
-    public ProduitEnSolde(){prime = 0.1;}
+    public ProduitEnSolde(int code, String des, double prix, String cat){
+        super(code, des, prix, cat);
+        prime = 0.1;
+    }
     public ProduitEnSolde(int code,String des, double prix,String cat, double prime){
         super(code,des,prix,cat);
         this.prime = prime;
